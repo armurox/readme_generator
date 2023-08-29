f057ac30e81e2eda05758c56d95a1f4e728138f6
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-30 00:03:01 +0400

    OpenAI key removed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     if len(sys.argv) != 2:
         sys.exit("Please provide a filename")
-    openai.api_key = "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" # Provide your openai API key
+    openai.api_key = "" # Provide your openai API key
     try:
         with open(sys.argv[1], "r") as file:
             prompt = f"Please generate a README for the following code: {file.read()}, with a summary of the programme, its features, the requirements and also how to run it" 
