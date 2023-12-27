daa36c622682a199592793ac5ebd53f344f5cfc3
Arman Jasuja <arman_jasuja@yahoo.com>
2023-12-28 00:13:40 +0400

    API Key sample

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import sys
 def main():
     if len(sys.argv) != 2:
         sys.exit("Please provide a filename")
-        # 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
-        # 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
-    openai.api_key = "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" # Provide your openai API key
+    openai.api_key = "" # Provide your openai API key
     try:
         with open(sys.argv[1], "r") as file:
             prompt = f"Please generate a README for the following code: {file.read()}" 
